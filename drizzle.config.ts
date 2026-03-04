df6859b9821f856ffbb19f3d9581e828d35a8e17
Bishouy Toubia <bishouytoubia@MacBookAir.fritz.box>
2026-03-04 17:24:27 +0100

    Update drizzle.config.ts for Turso support

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "file:sqlite.db",
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 });
